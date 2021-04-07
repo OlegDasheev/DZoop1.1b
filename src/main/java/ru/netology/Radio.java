e5a9ef5d32d2b17781d50514c3252ec7c21e83f1
Oleg <odasheyev@mail.ru>
2021-04-07 22:26:47 +0800

    Init commit

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,74 +1,15 @@
 package ru.netology;
 
 public class Radio {
+    private String name;
     private int currentRadioStation;
     private boolean on;
-    private int maxRadioStation = 9;
+    private int maxRadioStation = 10;
     private int minRadioStation = 0;
     private int currentVolume;
-    private int maxVolume = 10;
+    private int maxVolume = 100;
     private int minVolume = 0;
 
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
+    
 
-    public void setCurrentVolume(int currentVolume) {
-        this.currentVolume = currentVolume;
-    }
-
-    public void changeNextRadioStation() {
-        currentRadioStation++;
-        if (currentRadioStation > maxRadioStation) {
-            currentRadioStation = minRadioStation;
-        }
-    }
-
-    public void changePrevRadioStation() {
-        currentRadioStation--;
-        if (currentRadioStation < minRadioStation) {
-            currentRadioStation = maxRadioStation;
-        }
-    }
-
-    public void volumeUp() {
-        if (currentVolume >= maxVolume) {
-            return;
-        }
-        currentVolume++;
-    }
-
-    public void volumeDown() {
-        if (currentVolume <= minVolume) {
-            return;
-        }
-        currentVolume--;
-    }
-
-
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-
-
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
-
-    public int getCurrentRadioStation() {
-        return currentRadioStation;
-    }
-
-    public void setCurrentRadioStation(int currentRadioStation) {
-        this.currentRadioStation = currentRadioStation;
-    }
-
-
-    public void setMaxRadioStation(int maxRadioStation) {
-        this.maxRadioStation = maxRadioStation;
-    }
-
-    public void setMinRadioStation(int minRadioStation) {
-        this.minRadioStation = minRadioStation;
-    }
 }
